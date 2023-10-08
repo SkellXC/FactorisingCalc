2d8d88de52f827d6a6e2d05933fb5231cdeef941
Ethan Travasso <flameboyepic@gmail.com>
2023-10-08 19:29:24 +0100

    Refactored

--- factoriser.py
+++ factoriser.py
@@ -1,13 +1,10 @@
-go = True
-while go == True:
-    num = input("Enter number to factorise: ")
-    num = int(num)
-    print("The factors of", num,"are:")
+while True:
+    num = int(input("Enter number to factorise: "))
+    print(f"The factors of {num} are:")
     for x in range(1,num):
         for i in range(1,num):
             if x*i == num:
                 print(x,"|",i)
-            else:
-                continue
+
 
 
